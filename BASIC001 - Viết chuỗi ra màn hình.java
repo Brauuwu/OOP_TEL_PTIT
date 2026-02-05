56820fb658a4e436806ae55f936953c38031e7dd
Brauuwu <survivalminecraft080@gmail.com>
2026-02-05 18:08:47 +0700

    BASIC001 - Viết chuỗi ra màn hình

---
+++ BASIC001 - Viết chuỗi ra màn hình.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BASIC001{
+    public static void main(String[] args) {
+        Scanner sc;
+        sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+        while (t-- > 0) {
+            String s = sc.nextLine();
+            System.out.println(s);
+        }
+        sc.close();
+    }
+}
