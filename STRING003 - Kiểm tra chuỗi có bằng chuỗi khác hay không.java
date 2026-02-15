86e1ad45a4a315b9fe805f0f393815bca551adda
Brauuwu <survivalminecraft080@gmail.com>
2026-02-15 15:21:04 +0700

    STRING003 - Kiểm tra chuỗi có bằng chuỗi khác hay không

---
+++ STRING003 - Kiểm tra chuỗi có bằng chuỗi khác hay không.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class STRING003 {
+    public static void main(String[] args) {
+        Scanner sc;
+        sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+        while(t-- > 0){
+            String s1 = sc.nextLine().toLowerCase();
+            String s2 = sc.nextLine().toLowerCase();
+
+            System.out.println(s1.equals(s2));
+        }
+        sc.close();
+    }
+}
