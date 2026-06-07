0b1d55ec5a723972306450b4348722846b60a466
vhiu <survivalminecraft080@gmail.com>
2026-06-07 21:44:07 +0700

    EXCEPTION009 - Method Call Error

---
+++ EXCEPTION009 - Method Call Error.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+class MyMethods {
+    public static void m(int x) {
+        try {
+            m2(x);
+            System.out.println(1);
+        }
+        catch (ArithmeticException e) {
+            System.out.println(3);
+        }
+        catch (Exception e) {
+            System.out.println(2);
+        }
+    }
+
+    public static void m2(int x) throws IOException {
+        System.out.println(4);
+        if (x == 1)
+            throw new IOException();
+        if (x == 0)
+            throw new ArithmeticException();
+        System.out.println(5);
+    }
+}
+
+public class EXCEPTION009 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if(x == 0) MyMethods.m(1);
+        if(x == 1) MyMethods.m(0);
+        if(x == 2) MyMethods.m(2);
+    }
+}
