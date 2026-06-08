8b7dbe56d01a852dd49337f4bafe700b2b8fc6fb
vhiu <survivalminecraft080@gmail.com>
2026-06-08 21:18:00 +0700

    LAB01_BigDec - Big Decimal

---
+++ LAB01_BigDec - Big Decimal.java
@@ -0,0 +1,22 @@
+import java.math.*;
+import java.io.*;
+import java.util.*;
+
+public class LAB01_BigDec {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+
+        while (t-- > 0) {
+            BigDecimal sum = BigDecimal.ZERO;
+            String[] numbers = sc.nextLine().split(" ");
+            for (String number : numbers) {
+                String[] parts = number.split("\\.");
+                sum = sum.add(new BigDecimal(number).subtract(new BigDecimal(parts[0])));
+            }
+            System.out.println(sum);
+        }
+        sc.close();
+    }
+}
