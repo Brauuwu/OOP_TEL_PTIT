ae150f2fc90a31f57fd1591a1cb60f0eecadc030
vhiu <survivalminecraft080@gmail.com>
2026-06-08 16:41:05 +0700

    COLLECTION002 - Nhà kho nâng cao

--- COLLECTION002 - Nhà kho nâng cao.java
+++ COLLECTION002 - Nhà kho nâng cao.java
@@ -1,54 +1,48 @@
 import java.util.*;
+import java.io.*;
+import java.math.*;
 
-class Warehouse {
-    private HashMap<String, Integer> prices;
-    private HashMap<String, Integer> stocks;
-
-    public Warehouse() {
-        prices = new HashMap<>();
-        stocks = new HashMap<>();
+class Warehouse{
+    private HashMap<String, Integer> productsPrice;
+    private HashMap<String, Integer> productsStock;
+    
+    public Warehouse(){
+        productsPrice = new HashMap<>();
+        productsStock = new HashMap<>();
     }
-
-    public void addProduct(String product, int price, int stock) {
-        prices.put(product, price);
-        stocks.put(product, stock);
+    
+    public void addProduct(String product, int price, int stock){
+        productsStock.put(product, stock);
     }
-
-    public int price(String product) {
-        if (prices.containsKey(product))
-            return prices.get(product);
-        return -99;
+    
+    public int price(String product){
+        return productsPrice.containsKey(product) ? productsPrice.get(product) : -99;
     }
-
-    public int stock(String product) {
-        if (stocks.containsKey(product))
-            return stocks.get(product);
-        return 0;
+    
+    public int stock(String product){
+        return productsStock.containsKey(product) ? productsStock.get(product) : 0;
     }
-
-    public boolean take(String product) {
-        if (!stocks.containsKey(product) || stocks.get(product) <= 0)
-            return false;
-
-        stocks.put(product, stocks.get(product) - 1);
-        return true;
+    
+    public boolean take(String product){
+        int current = stock(product);
+        productsStock.replace(product, (current - 1) >= 0 ? current - 1 : 0);
+        return current > 0;
     }
 }
 
-public class COLLECTION002 {
-    public static void main(String[] args) {
-        Warehouse warehouse = new Warehouse();
-
+public class COLLECTION002{
+    public static void main(String[] args){
+    	Warehouse warehouse = new Warehouse();
         warehouse.addProduct("coffee", 5, 1);
-
+        
         System.out.println("stock:");
         System.out.println("coffee:  " + warehouse.stock("coffee"));
         System.out.println("sugar: " + warehouse.stock("sugar"));
-
+        
         System.out.println("taking coffee " + warehouse.take("coffee"));
         System.out.println("taking coffee " + warehouse.take("coffee"));
         System.out.println("taking sugar " + warehouse.take("sugar"));
-
+        
         System.out.println("stock:");
         System.out.println("coffee:  " + warehouse.stock("coffee"));
         System.out.println("sugar: " + warehouse.stock("sugar"));
